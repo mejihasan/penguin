999638f3faf913739aff6127fd35fce627017bfa
mejihasan <106650384+mejihasan@users.noreply.github.com>
2023-04-18 15:38:27 +0900

    Add files via upload

---
+++ クラブ/club_script.js
@@ -0,0 +1,8 @@
+$('.center').slick({
+    infinite: true,
+    dots: true,
+    centerMode: true, //要素を中央寄せ
+    centerPadding: '10%', //両サイドの見えている部分のサイズ
+    autoplay: true,
+    speed: 500,
+  });
