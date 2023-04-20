c7778fe04ad6e63d404bc96d16df3a7d58bab227
nakamura1523 <106650640+nakamura1523@users.noreply.github.com>
2023-04-20 13:37:44 +0900

    Delete club_script.js

--- クラブ/club_script.js
+++
@@ -1,8 +0,0 @@
-$('.center').slick({
-    infinite: true,
-    dots: true,
-    centerMode: true, //要素を中央寄せ
-    centerPadding: '10%', //両サイドの見えている部分のサイズ
-    autoplay: true,
-    speed: 500,
-  });
